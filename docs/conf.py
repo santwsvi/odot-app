64f3f080b56000b5b88c45fd3db8fd2882512c37
Victor Gabriel Santos Rocha <victorgsantosrocha@gmail.com>
2024-08-24 03:30:17 -0300

    docs: adding readthedocs theme for odot documentation

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,15 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
 ]
+
+html_theme = "sphinx_rtd_theme"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
